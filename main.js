592775448a3a78e35c5dffa56e5987f3ec10d1e7
Raptor <vvs.20073@mail.ru>
2024-10-15 06:08:25 -1200

    дз

---
+++ main.js
@@ -0,0 +1,84 @@
+// Declare a variable "a" and set it to the number 1
+var a = 1;
+
+// Declare a variable "b" and set it to the number 2.2
+
+var b = 2.2;
+// Declare a variable "c" and set it to the number 5000
+
+var c = 5000;
+// Declare a variable "d" and set it to the number 900000
+
+var d = 900000;
+// Declare a variable "e" and set it to the number 111222333444
+
+var e = 111222333444;
+// Declare a variable "isHavingFun" and set it to the boolean true
+
+let isHavingFun = true;
+// Declare a variable "isDifficult" and set it to the boolean false
+
+let isDifficult = false;
+// Declare a variable "letterA" and set it to the string "a"
+
+let letterA = "a";
+// Declare a variable "word" and set it to the string "banana"
+
+let word = "banana";
+// Declare a variable "sentence" and set it to the string "I ate breakfast."
+
+let sentence = "I ate breakfast."
+// Declare a variable "sentence" and set it to the string "The code
+// in the computer is so hard to understand. It is like magic. If I keep studying maybe one day I'll know."
+
+
+
+
+// var a;
+//let, const - способы объявления переменных
+// b - название переменной
+// = оператор присваивания
+// 2 - значение переменной
+
+// Типы данных примитивные
+
+// String - текстовые данные, строки
+
+// number (integer) - числовые
+
+// boolean - логические значения True False
+
+// undefined - неопределенное значение
+
+
+// null - пустое значение
+
+// symbol - айдишка уникальная
+
+// Типы данных объектные
+// Object
+// array
+// function
+
+//Операторы математические
+// + сложение
+// - вычитание
+// * умножение
+// / деление
+// % остаток от деления
+// ** возведение в степень
+
+
+// let x = 5;
+// let y = 3;
+// let sum;
+// sum = x ** y;
+// console.log(sum);
+
+// alert("Hello world");
+// console.log("Hello world");
+let name = prompt("Введите ваше имя");
+let surname = prompt("Введите фашу фамилию")
+let age = prompt("Введите ваш возраст");
+let city = prompt("Где вы живете?");
+alert("Вы " + name + " " + surname + " " + age + " " + "я живу в " + city);
